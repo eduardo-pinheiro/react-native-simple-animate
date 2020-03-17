4c1410cf100a45cdce180415cb58f4ace0dae2e5
Eduardo Pinheiro <38962010+eduardo-pinheiro@users.noreply.github.com>
2020-03-17 19:00:13 -0300

    create expo example

---
+++ example/src/Index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default class Index extends React.Component<any, any>{
+  render() {
+    return (
+      <Text>Hello World</Text>
+    )
+  }
+}
