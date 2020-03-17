ed05cdfd6a0d5849be5c1d5d65ef02da7e537f47
Eduardo Pinheiro <38962010+eduardo-pinheiro@users.noreply.github.com>
2020-03-17 19:17:11 -0300

    finish Boilerplate

--- example/src/Index.tsx
+++ example/src/Index.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { SimpleTest } from './reactComponentLib';
+import { View, StyleSheet } from 'react-native';
 
 export default class Index extends React.Component<any, any>{
   render() {
     return (
-      <Text>Hello World</Text>
+      <View style={styles.container}>
+        <SimpleTest
+          text="Funcionaaandu"
+        />
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  }
+});
