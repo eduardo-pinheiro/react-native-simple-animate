fe8d82b01997318ea264642fbe9ec9263fdbae2a
Eduardo Pinheiro <38962010+eduardo-pinheiro@users.noreply.github.com>
2020-04-07 13:17:54 -0300

    Update example

--- example/src/Index.tsx
+++ example/src/Index.tsx
@@ -1,36 +1,61 @@
 import React from 'react';
 import { Animate } from './reactComponentLib';
-import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, StatusBar } from 'react-native';
 
 export default class Index extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     this.state = {
-      isVisible: false,
+      isVisibleTranslate: false,
+      isVisiblePushFlex: false,
     };
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={() => this.setState({ isVisible: !this.state.isVisible })}>
-          <Text>{`click to ${this.state.isVisible ? 'hide' : 'show'}`}</Text>
-        </TouchableOpacity>
-        <View style={styles.wrapperAnimate}>
+        <View style={styles.translateWrapper}>
+          <TouchableOpacity
+            style={styles.translateButton}
+            onPress={() => this.setState({ isVisibleTranslate: !this.state.isVisibleTranslate })}
+          >
+            <Text>{`click to ${this.state.isVisibleTranslate ? 'hide' : 'show'}`}</Text>
+          </TouchableOpacity>
           <Animate
-            isVisible={this.state.isVisible} // Default appear behavior
-            animationType="slideRight" // Default "opacity"
+            isVisible={this.state.isVisibleTranslate} // Default appear behavior
+            animationType="slideUp" // Default "opacity"
             animationDelay="equalTransitionSpeed" // Default undefined
-            transitionSpeed="slow" // Default "regular"
+            transitionSpeed="fast" // Default "regular"
             movePoints={undefined} // Default AnimateConfig.movePoints
             disableOpacityAnimation={undefined} // Default false
             neverRemoveFromRender={undefined} // Default false
             animateCallbackFn={isVisibleInRender => console.log('isVisibleInRender:', isVisibleInRender)} // Default undefined
-            /*View Props*/
-            style={styles.animate}
           >
-            <View style={styles.textWrapper}>
-              <Text style={styles.text}>CONTEND</Text>
+            <View style={styles.translateContent}>
+              <Text style={styles.translateContentText}>CONTEND</Text>
+            </View>
+          </Animate>
+        </View>
+
+        <View style={styles.pushFlexWrapper}>
+          <TouchableOpacity
+            style={styles.pushFlexButton}
+            onPress={() => this.setState({ isVisiblePushFlex: !this.state.isVisiblePushFlex })}
+          >
+            <Text>{`click to ${this.state.isVisiblePushFlex ? 'hide' : 'show'}`}</Text>
+          </TouchableOpacity>
+          <Animate
+            isVisible={this.state.isVisiblePushFlex} // Default appear behavior
+            animationType="slideLeft" // Default "opacity"
+            // animationDelay="equalTransitionSpeed" // Default undefined
+            // transitionSpeed="slow" // Default "regular"
+            movePoints={110} // Default AnimateConfig.movePoints
+            disableOpacityAnimation={undefined} // Default false
+            neverRemoveFromRender={undefined} // Default false
+            animateCallbackFn={isVisibleInRender => console.log('isVisibleInRender:', isVisibleInRender)} // Default undefined
+          >
+            <View style={styles.pushFlexContent}>
+              <Text style={styles.pushFlexContentText}>CONTEND</Text>
             </View>
           </Animate>
         </View>
@@ -44,20 +69,55 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'space-evenly',
+    paddingTop: StatusBar.currentHeight,
   },
-  wrapperAnimate: {
+
+  translateWrapper: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  translateButton: {
+    width: 100,
     height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf1f4',
   },
-  animate: {
-    flex: 1,
+  translateContent: {
+    width: 100,
+    height: 100,
     backgroundColor: '#333333',
-    paddingHorizontal: 10,
-    paddingVertical: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  textWrapper: {
-    flex: 1,
-  },
-  text: {
+  translateContentText: {
     color: '#ffffff',
+    fontWeight: 'bold',
+  },
+
+  pushFlexWrapper: {
+    flex: 1,
+    height: 100,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  pushFlexButton: {
+    flex: 1,
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf1f4',
+  },
+  pushFlexContent: {
+    width: 100,
+    marginLeft: 10,
+    height: 100,
+    backgroundColor: '#333333',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pushFlexContentText: {
+    color: '#ffffff',
+    fontWeight: 'bold',
   },
 });
